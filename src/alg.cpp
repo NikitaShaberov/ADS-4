247ff6af72d340ee111e79152f9db887d118bc24
Aleksey-Chernyshev <99178326+Aleksey-Chernyshev@users.noreply.github.com>
2022-03-24 14:46:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,65 +1,66 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
 int par = 0;
-for (int i = 0; i < len-1; i++) {
-  for (int j = i+1; j < len; j++) {
-    if (arr[i] + arr[j] == value) {
+for (int i = 0; i < len - 1; i++) {
+  for (int j = i + 1; j < len; j++) {
+    if (arr[i] + arr[j] == value)
       par++;
-    }
   }
 }
 return par;
-return 0;
 }
 int countPairs2(int *arr, int len, int value) {
 int par = 0;
-int r;
-for (int i = len - 1; i >= 0; i--) {
-  if (arr[i] == value) {
-    r = i;
+int l = len;
+int r = len - 1;
+for (int z = len - 1; arr[l] > value; l--)
+  r--;
+for (int i = 0; i <= len; i++) {
+  if (arr[i] > value / 2)
     break;
-  } else {
-      continue;
-    }
-  }
-for (int i = 0; i < r - 1; i++) {
-  for (int j = r; j >= i + 1; j--) {
+  for (int j = r; j > i; j--) {
     if (arr[i] + arr[j] == value) {
       par++;
     }
+    if (arr[i] + arr[j] < value)
+      break;
   }
 }
 return par;
-return 0;
 }
 int countPairs3(int *arr, int len, int value) {
 int par = 0;
-int l = 0;
-int r = len-1;
+int r = len - 1;
 for (int i = 0; i < len - 1; i++) {
-  while (l < r) {
-    int mid = (l + r) / 2;
-    if (arr[i] + arr[mid] == value) {
+  bool res = false;
+  int l = i + 1;
+  int mid = (l + r) / 2;
+  while (l <= r) {
+    if (arr[mid] == value - arr[i]) {
       par++;
-      int j = mid + 1;
-      while (arr[i] + arr[j] == value && j < r) {
-        par++;
-        j++;
-      }
-      j = mid - 1;
-      while (arr[i] + arr[j] == value && j > l) {
-        par++;
-        j--;
-      } 
+      res = true;
       break;
+    } else
+      if (arr[mid] > value - arr[i]) {
+        r = mid - 1;
+      } else
+        if (arr[mid] < value - arr[i]) {
+          l = mid + 1;
+        }
+      mid = (l + l) / 2;
+  }
+  int cur1 = mid;
+  int cur2 = mid;
+  if (res == true) {
+    while (arr[cur1 - 1] == arr[mid] && cur1 - 1 != i) {
+      par++;
+      cur1--;
     }
-    if (arr[i] + arr[mid] > value) {
-      r = mid;
-    } else {
-        l = mid;
-      }
+    while (arr[cur2 + 1] == arr[mid]) {
+      par++;
+      cur2++;
+    }
   }
 }
 return par;
-return 0;
 }

f5a0ada5fdf45e7c3bb95fc19449aa5efb5feb3a
NikitaShaberov <99290177+NikitaShaberov@users.noreply.github.com>
2022-06-10 23:57:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,64 +1,47 @@
 // Copyright 2021 NNTU-CS
+int binar(int *arr, int l, int r, int a) {
+    int left = l, right = r, mid = 0; bool f = 0;
+    while (left <= right)  {
+        mid = (left + right) / 2;
+        if (arr[mid] == a) {
+            return binar(arr, l, mid - 1, a) + binar(arr, mid + 1, r, a) + 1;
+        }
+        if (a < arr[mid])
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return 0;
+}
+
 int countPairs1(int *arr, int len, int value) {
-int par = 0;
-for (int i = 0; i < len - 1; i++) {
-  for (int j = i + 1; j < len; j++) {
-    if (arr[i] + arr[j] == value)
-      par++;
+  int count1 = 0;
+  for (int i = 0; i < len; i++) {
+    for (int k = 0; k < len; k++) {
+      if ((i != k) && (arr[i] + arr[k] == value)) {
+        count1 += 1;
+      }
+    }
   }
+  return count1 / 2;
 }
-return par;
-}
+
 int countPairs2(int *arr, int len, int value) {
-int par = 0;
-int l = len;
-int r = len - 1;
-for (int z = len - 1; arr[l] > value; l--)
-  r--;
-for (int i = 0; i <= len; i++) {
-  if (arr[i] > value / 2)
-    break;
-  for (int j = r; j > i; j--) {
-    if (arr[i] + arr[j] == value) {
-      par++;
+  int count2 = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int k = len - 1; k > i; k--) {
+      if (arr[i] + arr[k] == value) {
+        count2 += 1;
+      }
     }
-    if (arr[i] + arr[j] < value)
-      break;
   }
+  return count2;
 }
-return par;
-}
+
 int countPairs3(int *arr, int len, int value) {
-int par = 0;
-int r = len - 1;
-for (int i = 0; i < len - 1; i++) {
-  bool res = false;
-  int l = i + 1;
-  int mid = (l + r) / 2;
-  while (l <= r) {
-    if (arr[i] + arr[mid] == value) {
-      par++;
-      res = true;
-      break;
-    } else if (arr[mid] > value - arr[i]) {
-      r = mid - 1;
-    } else if (arr[mid] < value - arr[i]) {
-      l = mid + 1;
-    }
-    mid = (l + r) / 2;
+  int count3 = 0;
+  for (int i = 0; i < len; i++) {
+      count3 += binar(&arr[i + 1], 0, len - i, value - arr[i]);
   }
-  int cur1 = mid;
-  int cur2 = mid;
-  if (res == true) {
-    while (arr[cur1 - 1] == arr[mid] && cur1 - 1 != i) {
-      par++;
-      cur1--;
-    }
-    while (arr[cur2 + 1] == arr[mid]) {
-      par++;
-      cur2++;
-    }
-  }
-}
-return par;
+  return count3;
 }
